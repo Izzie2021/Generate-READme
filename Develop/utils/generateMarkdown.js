cb21dcfd9eebcf6b19d4a45bd079d7ea144bd3e4
Izzie <ismael.espana88@gmail.com>
2022-02-21 23:28:00 -0700

    added code to markdown and updated READme

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,32 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  let licenseType = license.license; // i think this is problem #1
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license]`
+  } else {
+    license.license = " "
+  }
+  return yourLicense;
+};
+
+
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
