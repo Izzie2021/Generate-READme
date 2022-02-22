7cf47beca50451abc548827942d06c4311531d8b
Izzie <ismael.espana88@gmail.com>
2022-02-21 23:33:16 -0700

    updated index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+package.json
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Say hello to your READme generator! Provide your full name:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('A name must be entered!');
+                return false;
+            }
+        }
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
-// Function call to initialize app
 init();
